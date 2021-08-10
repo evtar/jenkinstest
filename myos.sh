7f1f79b03a73bc8ec0541dd0559f8af57f5a03af
evtar <88701536+evtar@users.noreply.github.com>
2021-08-10 09:45:03 +0400

    Add files via upload

---
+++ myos.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mynamecomputer="acer"
+myos=`uname`
+echo "тест скрипта $0"
+echo "Модель компьтера $mynamecomputer"
+echo "ОС комьюетра $myos"
+
+num1=10
+num2=10
+summ=$((num1*num2))
+echo "Умножим $num1 на $num2 и получим $summ"
